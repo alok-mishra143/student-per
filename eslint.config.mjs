8eb2942d32dc0541cdce0631194f2225c72caa77
Alok Mishra <21se02cs042@ppsu.ac.in>
2025-02-11 10:03:16 +0530

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "warn", // Warn instead of error for console logs
+      "no-debugger": "error", // Prevent accidental debugger statements
+      "@next/next/no-img-element": "off", // Allow <img> if needed
+      "react/jsx-key": "warn", // Warn if keys are missing in lists
+    },
+  },
 ];
 
 export default eslintConfig;
